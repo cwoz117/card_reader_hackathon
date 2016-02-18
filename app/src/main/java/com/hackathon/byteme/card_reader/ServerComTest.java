89748a19a81b06e42635782e83dbd2019a2f359f
jeremy mah <jeremyw.mah@gmail.com>
2016-02-18 02:12:58 -0700

    adding a func test file

---
+++ app/src/main/java/com/hackathon/byteme/card_reader/ServerComTest.java
@@ -0,0 +1,13 @@
+package com.hackathon.byteme.card_reader;
+
+
+/**
+ * Created by bighoon on 2016-02-18.
+ */
+public class ServerComTest {
+    public static void main(String args[])
+    {
+        Server_com com = new Server_com();
+        com.send("12345678 butts");
+    }
+}
